ecd08aee122edf6df87216747b230080e90befd7
Joel Jose <mail@joeljose.dev>
2023-04-02 14:52:18 +0530

    Implemented new Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,18 @@
+import Link from "next/link";
 import styles from "../styles/pages/index.module.css";
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      <form action="/" method="get">
-        <p className={styles.inputLabel}>Enter Vehicle Number</p>
-        <input
-          className={styles.input}
-          name="vehicle"
-          type="text"
-          minLength={10}
-          maxLength={10}
-          placeholder="Eg. KL07AV5510"
-          autoFocus
-          required
-        />
-        <button className={styles.submit} type="submit">
-          Submit
-        </button>
-      </form>
+      <div className={styles.title}>Welcome!</div>
+      <div className={styles.buttonContainer}>
+        <Link href="/login">
+          <div className={styles.label}>Login</div>
+        </Link>
+        <Link href="/register">
+          <div className={styles.label}>Register</div>
+        </Link>
+      </div>
     </div>
   );
 };
